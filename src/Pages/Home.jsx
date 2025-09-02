4da69ecfdf43fa4e064a5c6736f88b015afde421
Pranita-Sarjerao-Thorat <pranita662003@gmail.com>
2025-09-02 12:13:19 +0530

    2026

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 export default function Home() {
   return (
     <div className="text-center mt-12">
@@ -8,7 +9,7 @@ export default function Home() {
       <p className="text-gray-600 mt-2">
         Find your perfect match and get started today!
       </p>
-
+      
       <div className="mt-6">
         <Link to="/login">
           <button className="mr-3 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
